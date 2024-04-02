2c216c390988389f8d2e6e29b78b58e60f53f03b
Egor Mishin <em@egormishin.com>
2024-04-02 16:35:41 +0200

    cofig: add superbase config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
 export default function (api) {
-	api.cache(true);
-	return {
-		presets: ['babel-preset-expo'],
-	};
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
+  };
 }
